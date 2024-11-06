c3d9e283a7d89f86ca36760d80cd670235ea659f
DESKTOP-I176V4D\Mihai <0ursachemihaiandrei0@gmail.com>
2024-11-07 00:08:59 +0200

    -Used HarshRouter for multiple pages

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom'
+    HashRouter as Router, // Change BrowserRouter to HashRouter
+    Route,
+    Switch,
+    Redirect,
+} from 'react-router-dom';
 
-import './style.css'
-import Home from './views/home'
-import About from './views/about'
-import Contact from './views/contact'
-import NotFound from './views/not-found'
+import './style.css';
+import Home from './views/home';
+import About from './views/about';
+import Contact from './views/contact';
+import NotFound from './views/not-found';
 
 const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route component={Home} exact path="/momentum-site/" />
-        <Route component={About} exact path="/momentum-site/about" />
-        <Route component={Contact} exact path="/momentum-site/contact" />
-        <Route component={NotFound} path="**" />
-        <Redirect to="**" />
-      </Switch>
-    </Router>
-  )
-}
+    return (
+        <Router>
+            <Switch>
+                <Route component={Home} exact path="/" />  {/* Adjusted paths for HashRouter */}
+                <Route component={About} exact path="/about" />
+                <Route component={Contact} exact path="/contact" />
+                <Route component={NotFound} />
+                <Redirect to="/" />
+            </Switch>
+        </Router>
+    );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
