d3f9b75fb9c82a0435acaee867f8e954f1d66a6d
UrsacheMihai <0ursachemihaiandrei0@gmail.com>
2024-11-10 04:48:28 +0200

    -About Page Initiated

--- src/components/src/views/about.js
+++ src/components/src/views/about.js
@@ -14,7 +14,10 @@ import misu from '../Assets/Misu.jpg'
 import mihai from '../Assets/Mihai.jpg'
 import bia from '../Assets/Bia.jpg'
 import hagiul from '../Assets/Hagiul.jpg'
+import Darian from '../Assets/Darian.jpg'
 import ScrollToTop from '../../scroll';
+import ftc from '../Assets/FTC logo.png'
+import {LinearTextGradient, RadialTextGradient} from "react-text-gradients-and-animations";
 const About = (props) => {
   return (
       <div className="about-container">
@@ -50,23 +53,66 @@ const About = (props) => {
           ></Navbar8>
 
           <div className="Our_Team">
-              <span className="Our_Team">Our Team</span>
+              <span className="Our_Team"><LinearTextGradient angle={45} colors={["#000C40 5%","#607D8B"]}>Our Team</LinearTextGradient></span>
           </div>
           <div className="Leaders">
-              <span className="Leaders">Leaders</span>
+              <span className="Leaders"><LinearTextGradient angle={45} colors={["#780206","#601161"]}>Main Leader</LinearTextGradient></span>
           </div>
-          <div className="Picture and name">
-              <img className="image" src={bia}></img>
-              <span className="hide">Bianca</span>
-              <img className="image" src={bia}></img>
-              <span className="hide">Bianca</span>
-              <img className="image" src={bia}></img>
-              <span className="hide">Bianca</span>
+          <div className="picture-and-name1">
+              <div className="image-container">
+                  <img className="image1" src={bia} alt="Bianca"/>
+                  <span className="hide1">Jercan Bianca</span>
+                  <span className="hide2">Lider Principal</span> {/* Department Text */}
+              </div>
           </div>
-          
-
-
-
+          <div className="Leaders">
+              <span className="Leaders"><LinearTextGradient angle={45} colors={["#185a9d","#43cea2"]}>Departament</LinearTextGradient></span>
+          </div>
+          <div className="Leaders">
+              <span className="Leaders"><LinearTextGradient angle={45} colors={["#185a9d","#43cea2"]}>Leaders</LinearTextGradient></span>
+          </div>
+          <div className="picture-and-name1">
+              <div className="image-container">
+                  <img className="image1" src={hagiul} alt="Hagiul"/>
+                  <span className="hide1">Haigu Andrei</span> {/* Name Text */}
+                  <span className="hide2">Co-Lider Constructie</span> {/* Department Text */}
+              </div>
+              <div className="image-container">
+                  <img className="image1" src={misu} alt="Misu"/>
+                  <span className="hide1">Mihai</span> {/* Name Text */}
+                  <span className="hide2">Lider Programare  Co-Lider Constructie</span> {/* Department Text */}
+              </div>
+              <div className="image-container">
+                  <img className="image1" src={mihai} alt="Mihai"/>
+                  <span className="hide1">Ursache Mihai Andrei</span> {/* Name Text */}
+                  <span className="hide2">Lider Web Design</span> {/* Department Text */}
+              </div>
+              <div className="image-container">
+                  <img className="image1" src={ftc} alt="Bianca"/>
+                  <span className="hide1">To Be Comunicated</span> {/* Name Text */}
+                  <span className="hide2">Marketing</span> {/* Department Text */}
+              </div>
+              <div className="image-container">
+                  <img className="image1" src={Darian} alt="Bianca"/>
+                  <span className="hide1">Darian</span> {/* Name Text */}
+                  <span className="hide2">Jurnal</span> {/* Department Text */}
+              </div>
+              <div className="image-container">
+                  <img className="image1" src={ftc} alt="Bianca"/>
+                  <span className="hide1">To Be Comunicated</span> {/* Name Text */}
+                  <span className="hide2">Proiectare</span> {/* Department Text */}
+              </div>
+              <div className="image-container">
+                  <img className="image1" src={ftc} alt="Bianca"/>
+                  <span className="hide1">To Be Comunicated</span> {/* Name Text */}
+                  <span className="hide2">Design</span> {/* Department Text */}
+              </div>
+              <div className="image-container">
+                  <img className="image1" src={ftc} alt="Bianca"/>
+                  <span className="hide1">To Be Comunicated</span> {/* Name Text */}
+                  <span className="hide2">3D Design</span> {/* Department Text */}
+              </div>
+          </div>
           <Footer4
               link1={
                   <Fragment>
